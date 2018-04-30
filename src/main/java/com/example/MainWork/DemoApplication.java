52a812bdd78ec94cd1f03901112dc3e9fd9e86d8
iminib39 <iminib39@gmail.com>
2018-05-01 01:46:44 +0300

    alex3985

--- src/main/java/com/example/MainWork/DemoApplication.java
+++ src/main/java/com/example/MainWork/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.MainWork;
+package com.example.cource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	@RequestMapping("/hello")
+String hello(Map<String, Object> model) {
+    RelativisticModel.select();
+    String energy = System.getenv().get("ENERGY");
+    if (energy == null) {
+       energy = "12 GeV";
+    }
+    Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+    model.put("science", "E=mc^2: " + energy + " = "  + m.toString());
+    return "hello";
+}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
