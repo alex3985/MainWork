092f8487b7ae88f9e7aad1f546346f59d52750ef
iminib39 <iminib39@gmail.com>
2018-05-01 13:55:07 +0300

    alex3985

--- src/main/java/com/example/MainWork/controllers/HomeController.java
+++ src/main/java/com/example/MainWork/controllers/HomeController.java
@@ -4,10 +4,15 @@ package com.example.MainWork.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+
 @RestController
 public class HomeController {
     @RequestMapping("/hello")
-    public String home(){
-        return "Hello World!";
+    public LinkedList<String> home(){
+        LinkedList<String> a = new LinkedList<>();
+        a.add("asd");
+        a.add("werfg");
+        return a;
     }
 }
