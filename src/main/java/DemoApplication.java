c0c5b79207ed5aa9a546decc6a53454ed0e6032c
iminib39 <iminib39@gmail.com>
2018-05-01 13:27:05 +0300

    alex3985

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -1,16 +1,9 @@
-package com.example.MainWork;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/hello")
-       String hello() {
-    return "hello";
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
