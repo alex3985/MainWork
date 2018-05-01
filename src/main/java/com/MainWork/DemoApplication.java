8a9caab1b6f950df08b5c3e813f32f987a17e611
iminib39 <iminib39@gmail.com>
2018-05-01 16:04:48 +0300

    alex3985

--- src/main/java/com/MainWork/DemoApplication.java
+++ src/main/java/com/MainWork/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.MainWork;
+package com.MainWork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/MainWork/controllers/HomeController.java
+++ src/main/java/com/MainWork/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.MainWork.controllers;
+package com.MainWork.controllers;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
