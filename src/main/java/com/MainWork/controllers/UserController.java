aa45e838ad56c4bcfe501e5684334c52caf8913f
iminib39 <iminib39@gmail.com>
2018-05-02 21:09:14 +0300

    alex3985

--- src/main/java/com/MainWork/controllers/UserController.java
+++ src/main/java/com/MainWork/controllers/UserController.java
@@ -1,15 +1,11 @@
 package com.MainWork.controllers;
 
-import com.MainWork.database.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    DataSource dataSource;
     @RequestMapping("/authorization")
     public Object Authorizathion (){
         return 25;
