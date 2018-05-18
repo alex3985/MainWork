6ec9bae37f94b458dffd1ceed16ce7497fc2d381
iminib39 <iminib39@gmail.com>
2018-05-19 01:19:43 +0300

    alex3985

--- src/main/java/com/MainWork/controllers/ExamController.java
+++ src/main/java/com/MainWork/controllers/ExamController.java
@@ -23,7 +23,7 @@ public class ExamController {
     public Object getExamsById(@PathVariable(value = "id") int id) throws SQLException {
         Statement stm = dataSource.getConnection().createStatement();
         ResultSet rs = stm.executeQuery("SELECT name,measure,five,two,three,four,data FROM public.\"Standards\" INNER JOIN " +
-                "public.\"Exam\" ON public.\"Standards\".standardid=public.\"Exam\".standardid WHERE public.\"Standards\".standardid="+id);
+                "public.\"Exam\" ON public.\"Standards\".standardid=public.\"Exam\".standardid WHERE public.\"Exam\".studentid="+id);
         if(!rs.next()){
             rs.close();
             stm.close();
