86eb4e2ae92b28df23e4b16ebf934aa189154db8
iminib39 <iminib39@gmail.com>
2018-05-22 02:25:05 +0300

    alex3985

--- src/main/java/com/MainWork/controllers/CoachController.java
+++ src/main/java/com/MainWork/controllers/CoachController.java
@@ -53,7 +53,7 @@ public class CoachController {
     public Object getCoachesSrudentsById(@PathVariable(value = "id") int id) throws SQLException{
         Connection con = dataSource.getConnection();
         Statement stm = con.createStatement();
-        ResultSet rs = stm.executeQuery("SELECT studentid,journal.facultyid,surname,student.name,student.patronymic,public.\"Faculty\".name,student.group," +
+        ResultSet rs = stm.executeQuery("SELECT journal.studentid,journal.facultyid,surname,student.name,student.patronymic,public.\"Faculty\".name,student.group," +
                 "attendance,lastdate,student.phone,student.sex FROM journal INNER JOIN (public.student INNER JOIN public.\"Faculty\"" +
                 " ON student.facultyid=public.\"Faculty\".facultyid) ON journal.sectionid=student.studentid WHERE coachid="+id);
         if(!rs.next()){
