7c39864f54f00e41e2114fc203172f4d2fd4cee9
iminib39 <iminib39@gmail.com>
2018-05-28 16:29:06 +0300

    alex3985

--- src/main/java/com/MainWork/controllers/SectionController.java
+++ src/main/java/com/MainWork/controllers/SectionController.java
@@ -113,7 +113,7 @@ public class SectionController {
     public Object getSectionAndCoachByIdStudent(@PathVariable(value="id") int id) throws SQLException {
         Connection con = dataSource.getConnection();
         Statement stm = con.createStatement();
-        ResultSet rs = stm.executeQuery("SELECT section.name,coach.name,surname, patronomic FROM public.section INNER JOIN public.coach ON public.coach.coachid=public.section.coachid WHERE sectionid IN" +
+        ResultSet rs = stm.executeQuery("SELECT section.name,coach.name,surname, patronomic FROM public.section INNER JOIN public.coach ON public.coach.sectionid=public.section.sectionid WHERE sectionid IN" +
                 " (SELECT sectionid FROM public.journal WHERE studentid="+id+" )");
         if(rs.next()){
             Section section  = new Section(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
